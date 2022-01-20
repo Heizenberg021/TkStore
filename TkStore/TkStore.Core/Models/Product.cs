3042fe9ea26ccca6f58dc0f0ca943aa67c9343d1
Teboho Modise <Teboho@tokkie.onmicrosoft.com>
2022-01-20 12:18:18 +0200

    Added new Product and ProductRepository classes

---
+++ TkStore/TkStore.Core/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TkStore.Core.Models
+{
+    public class Product
+    {
+        public string ID { get; set; }
+        [StringLength(30)]
+        [DisplayName("ProductName")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(0,50000)]
+        public string Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+        public Product()
+        {
+            this.ID = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ TkStore/TkStore.Data.AccessInMemory/ProductRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using TkStore.Core.Models;
+
+namespace TkStore.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+    public ProductRepository()
+    {
+        products = cache["products"] as List<Product>;
+        if (products == null)
+            {
+                products = new List<Product>();
+            }
+    }
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(p => p.ID == product.ID);
+            if (productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+
+        public Product Find(string ID)
+        {
+            Product product = products.Find(p => p.ID == ID);
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+    }
+}
